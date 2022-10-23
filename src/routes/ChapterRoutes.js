d8380ed2cd451dea2148ca0826cb7afe2267083f
Mirco_874 <mirco.p@krugerns.com>
2022-10-22 22:48:32 -0400

    se agrego la lista de archivos de un capitulo

--- src/routes/ChapterRoutes.js
+++ src/routes/ChapterRoutes.js
@@ -4,6 +4,25 @@ import {Router} from 'express'
 const ChapterRoutes= Router();
 ChapterRoutes.get('/:id',(req,res)=>{
 
+    if(querySize!==0 ){
+        let query="SELECT * FROM capitulo WHERE id=? and ";
+        let values=[req.params.id];
+
+        const atributes=req.query;
+        for (const property in atributes) {
+
+            query+=property + "=? and " ;
+            values.push(atributes[property])
+
+            //el ultimo atriburo del query object
+            //que pasa si se inventan un atributo del query
+        }
+        query=query.substring(0,query.length-4);
+        console.log(query)
+        console.log(values)
+        const[rows, fields]=  dbConnection.query(query,[req.params.id]);
+        res.send("asd");
+    }
 
 })
 
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -9,15 +9,34 @@ router.get('/chapters',async (req,res)=>{
 })
 
 router.get('/chapters/:id/material',async (req,res)=>{
-    const[rows, fields]= await dbConnection.query(`
-    SELECT  capitulo.titulo_capitulo as titulo_capitulo,
-            capitulo.titulo_material as titulo_material,
-            capitulo.descripcion_material as descripcion_material,
-            archivo.enlace as enlace_material
-    FROM capitulo,archivo 
-    WHERE capitulo.id=? and 
-    capitulo.id=archivo.id_capitulo`, [req.params.id]);
-    res.send(rows);
+    const [rows,field]= await dbConnection.query(`
+    SELECT  capitulo.id as id_capitulo,
+            capitulo.titulo_capitulo,
+            capitulo.titulo_material,
+            capitulo.descripcion_material,
+            archivo.id as id_archivo,
+            archivo.tipo as tipo,
+            archivo.enlace as enlace_material,
+            archivo.contenido
+    FROM capitulo,archivo
+    WHERE capitulo.id=? and capitulo.id=archivo.id_capitulo`, [req.params.id]);
+    
+    const material=rows.map((item)=>{
+        return ({ 
+            id_archivo:item.id_archivo,
+            tipo:item.tipo,
+            enlace_material:item.enlace_material,
+            contenido:item.contenido })
+        });
+
+    const result={
+        id_capitulo:rows[0].id_capitulo,
+        titulo_capitulo:rows[0].titulo_capitulo,
+        titulo_material:rows[0].titulo_material,
+        descripcion_material:rows[0].descripcion_material,
+        material:material
+    };
+    res.send(result);
 })
 
 router.get('/chapters/:id/forum',async (req,res)=>{
@@ -27,10 +46,28 @@ router.get('/chapters/:id/forum',async (req,res)=>{
             capitulo.estado_foro as estado,
             capitulo.titulo_foro as titulo_foro,
             capitulo.descripcion_foro as descripcion_foro,
+            mensaje.id as id_mensaje,
+            mensaje.autor as autor,
             mensaje.contenido as mensaje
     FROM capitulo,mensaje 
     WHERE capitulo.id=? and capitulo.id=mensaje.id_capitulo`, [req.params.id]);
-    res.send(rows);
+    
+    const messages= rows.map((item)=>{
+        return ({
+                id_mensaje:item.id_mensaje,
+                autor:item.autor,
+                mensaje:item.mensaje
+        });
+    });
+    const result= {
+        titulo_capitulo: rows[0].titulo_capitulo,
+        titulo_material: rows[0].titulo_material,
+        estado: rows[0].estado,
+        titulo_foro: rows[0].titulo_foro,
+        descripcion_foro: rows[0].descripcion_foro,
+        mensajes: messages
+    }
+    res.send(result);
 })
 
 router.get('/chapters/:id/practice',async (req,res)=>{
